db0eed5e4cc939ea540856b57dedaa266dd4a35c
Kuba Jalowiec <kuba.jalowiec@protonmail.com>
2020-07-27 21:03:15 +0200

    Adding GPL header to every source file.

--- lang/en_us.php
+++ lang/en_us.php
@@ -1,5 +1,25 @@
 <?php
 
+/* 
+Author:  Jakub Jalowiec
+E-mail:  kuba.jalowiec@protonmail.com
+Website: https://github.com/kubajal/module-force_password_change
+This file is part of Force Password Change - a Formtools module.
+
+Force Password Change is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+Force Password Change is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Force Password Change.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 $L = array();
 
 // required fields
